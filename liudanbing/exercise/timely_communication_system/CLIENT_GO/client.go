74cba14a1e6b5920caf396e3b22cff3958e67173
DylanJinx <2636353989@qq.com>
2024-12-21 02:49:41 +0800

    v9_6

--- liudanbing/exercise/timely_communication_system/CLIENT_GO/client.go
+++ liudanbing/exercise/timely_communication_system/CLIENT_GO/client.go
@@ -24,7 +24,7 @@ type Client struct {
 	ServerPort int
 	Name	   string
 	conn       net.Conn
-	flag	   int // 当前client的模式
+	flag       int            // 当前client的模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -52,6 +52,7 @@ func (c *Client) menu() bool {
 	fmt.Println(">>>>>> 1. 公聊模式")
 	fmt.Println(">>>>>> 2. 私聊模式")
 	fmt.Println(">>>>>> 3. 更新用户名")
+	fmt.Println(">>>>>> 4. 查询在线用户")
 	fmt.Println(">>>>>> 0. 退出")
 
 	/*
@@ -83,7 +84,7 @@ func (c *Client) menu() bool {
 		return false
 	}
 
-	if flag >= 0 && flag <= 3 {
+	if flag >= 0 && flag <= 4 {
 		c.flag = flag
 		return true
 	} else {
@@ -106,6 +107,80 @@ func (c *Client) UpdateName() bool {
 	return true
 }
 
+// 公聊模式
+func (c *Client) PublicChat() {
+	fmt.Println(">>>>>> 请输入聊天内容，exit退出")
+	var chatMsg string
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		// 消息不为空则发送
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := c.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn.Write err:", err)
+				break
+			}
+		}
+
+		chatMsg = ""
+		fmt.Println(">>>>>> 请输入聊天内容，exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+// 查询在线用户
+func (c *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := c.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return
+	}
+}
+
+// 私聊模式
+func (c *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	c.SelectUsers()
+
+	fmt.Println(">>>>>> 请输入聊天对象用户名，exit退出:")
+	fmt.Scanln(&remoteName)
+
+	// 选择聊天对象
+	for remoteName != "exit" {
+		fmt.Println(">>>>>> 请输入消息内容，exit退出:")
+		fmt.Scanln(&chatMsg)
+
+		// 与这个聊天对象进行聊天
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := c.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn.Write err:", err)
+					break
+				}
+			}
+
+			// 多次输入消息
+			// chatMsg = ""
+			fmt.Println(">>>>>> 请输入消息内容，exit退出:")
+			fmt.Scanln(&chatMsg)
+		}
+
+		// 重新选择聊天对象
+		//remoteName = ""
+		c.SelectUsers()
+		fmt.Println(">>>>>> 请输入聊天对象用户名，exit退出:")
+		fmt.Scanln(&remoteName)
+
+	}
+}
+
 func (c *Client) Run() {
 	for c.flag != 0 { // 不断得循环，判断flag是否为0
 		for !c.menu() {} // 如果menu()返回false，那么就一直循环，直到menu()返回true
@@ -115,16 +190,21 @@ func (c *Client) Run() {
 		case 1:
 			// 公聊模式
 			fmt.Println(">>>>>> 公聊模式")
+			c.PublicChat()
 
 		case 2:
 			// 私聊模式
 			fmt.Println(">>>>>> 私聊模式")
+			c.PrivateChat()
 
 		case 3:
 			// 更新用户名
 			fmt.Println(">>>>>> 更新用户名")
 			c.UpdateName()
-
+		case 4:
+			// 查询在线用户
+			fmt.Println(">>>>>> 查询在线用户")
+			c.SelectUsers()
 		}
 	}
 }
@@ -132,13 +212,20 @@ func (c *Client) Run() {
 // 这段逻辑不能写到Run()中，如果写到Run()中，那么Run()就会阻塞在这里，无法继续执行
 func (c *Client) DealResponse() {
 	// 一旦client.conn有数据，就直接拷贝到os.Stdout标准输出上，永久阻塞监听
-	io.Copy(os.Stdout, c.conn)
+	_, err := io.Copy(os.Stdout, c.conn)
 	// 等价于：
 	// for {
 	// 	buf := make([]byte, 4096)
 	// 	n, err := c.conn.Read(buf)
 	// 	fmt.Println(buf[:n])
+
+	if err != nil {
+		fmt.Println("\n>>>>>> 与服务器的连接已断开，客户端即将退出...")
+	}
+	// 无论是正常EOF还是错误导致的退出，都结束客户端
+	os.Exit(0)
 }
+
 
 func main() {
 	// 命令行解析
